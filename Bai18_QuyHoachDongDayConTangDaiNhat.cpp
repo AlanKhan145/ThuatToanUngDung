f39c7a1a68f2adf8f70b1e1b4d919c6f00b7929d
KainTranSaiki <Khanhtranan56@gmail.com>
2024-12-30 16:25:42 +0700

    Trần An Khánh posted

---
+++ Bai18_QuyHoachDongDayConTangDaiNhat.cpp
@@ -0,0 +1,24 @@
+/*Quy hoạch động dãy con tăng dài nhất*/
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int n ;
+  cin>> n ;
+  vector<int> a(n+1);
+  for(int i = 1 ; i <= n ; i++ ){
+    cin >> a[i];
+  }
+  vector<int>d(n+1,0);
+  d[1] = 1 ;
+  int max_kc = 1;
+  for(int i = 2 ; i <= n ; i++){
+    for(int k = i - 1 ; k >= 1 ; k--){
+      if(a[k]<a[i]){
+        d[i] = max(d[i],d[k]+1);
+      }
+    }
+    max_kc = max(max_kc,d[i]);
+  }
+  cout << max_kc<<endl;
+  return 0;
+}
